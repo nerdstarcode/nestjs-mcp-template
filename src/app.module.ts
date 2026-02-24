4405bd5f60d18596c7bfcaba7d1c6c9a3808c72f
Sthiven <sthivendev@gmail.com>
2026-02-24 19:38:13 -0300

    :recycle: disable postgree mcp

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { _HealthResolverModule } from './modules/_HealthResolver/_HealthResolver.module';
 import { McpModule } from '@nestjs-mcp/server';
-import { _PostgreeModule } from './modules/_Postgree/_Postgree.module';
 import { _MongoModule } from './modules/_Mongo/_Mongo.module';
 import { ConfigModule } from '@nestjs/config';
 import { ModuleNameController } from './modules/ModuleName/modulename.controller';
@@ -19,14 +18,14 @@ import { TypegooseExampleModule } from './modules/TypegooseExample/typegoose-exa
     MongoInfrastructureModule,
     RedisModule,
     ConfigModule.forRoot({
-      isGlobal: true, // Makes the ConfigService available everywhere
-      envFilePath: '.env', // Optional: default is .env in the root
+      isGlobal: true,
+      envFilePath: '.env',
     }),
-    _HealthResolverModule,
-    _PostgreeModule,
     _MongoModule,
-    TypegooseExampleModule,
     _RedisClientModule,
+    _HealthResolverModule,
+    // _PostgreeModule, // not configured - insecure querys
+    TypegooseExampleModule,
     McpModule.forRoot({
       name: 'My MCP Server',
       version: '1.0.0',
